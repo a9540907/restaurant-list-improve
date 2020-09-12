37b82ef36bab3b8970cd04bd6cf7734f3dc6c251
stan_wang <a7569209@gmail.com>
2020-09-13 01:44:46 +0800

    feat: add method-override

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+
+const restaurantJson = require('../../restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurant-list-again', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < restaurantJson.results.length; i++) {
+
+    Restaurant.create({
+      id: `${restaurantJson.results[i].id}`,
+      name: `${restaurantJson.results[i].name}`,
+      name_en: `${restaurantJson.results[i].name_en}`,
+      category: `${restaurantJson.results[i].category}`,
+      image: `${restaurantJson.results[i].image}`,
+      location: `${restaurantJson.results[i].location}`,
+      phone: `${restaurantJson.results[i].phone}`,
+      google_map: `${restaurantJson.results[i].google_map}`,
+      rating: `${restaurantJson.results[i].rating}`,
+      description: `${restaurantJson.results[i].description}`,
+    })
+  }
+  console.log('done')
+})
